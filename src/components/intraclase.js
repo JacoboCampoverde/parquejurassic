5362a88068b6fb1c6c2e110c7d0308380827d6fb
JacoboCampoverde <jacobocames17@gmail.com>
2024-04-21 21:11:20 -0500

    Se agrega

---
+++ src/components/intraclase.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+
+const Intraclase=()=> {
+  return (
+    <div style={{ backgroundImage: 'url(https://i.pinimg.com/originals/9a/ac/64/9aac640fba60a5a553d945348ca0d251.jpg)', backgroundSize: 'cover', minHeight: '100vh' }}>
+      <h1>
+
+      <div id="fondo">
+
+      <div id="encabezado">
+        <div className="utmach" style={{ textAlign: 'center', paddingTop: '1%' }}>
+          <img src="https://www.utmachala.edu.ec/portalwp/wp-content/uploads/2015/08/LOGOINTRO1.png" alt="" style={{ width: '400px' }} />
+        </div>
+      </div>      
+      
+      <div className="contenedor">
+        <div className="seccion1">
+          <h1 className="titulo" style={{ justifyContent: 'center', fontSize: '30px',textAlign: 'center', fontFamily: 'Times New Roman, Times, serif', border: '2px solid black', borderRadius: '10px' }}>Jurassic Park</h1>
+          <div className="photo" style={{ textAlign: 'center', paddingTop: '1%' }}>
+           <img src="https://brandemia.org/sites/default/files/sites/default/files/jurassic-park-logo-historia-parque-jurasico.jpg" alt="" style={{ width: '200px', border: '2px solid black' }} />
+          </div>
+        </div>
+        <hr></hr>
+
+        <div className="seccion2">
+          <h1 style={{ justifyContent: 'center', textAlign: 'center', fontSize: '30px', fontFamily: 'Times New Roman, Times, serif', fontWeight: 'bold'}}> Sinopsis</h1>
+          <p>
+           <b style={{ justifyContent: 'center', fontSize: '15px', fontFamily: 'Georgia, serif' }}>En la isla Nublar, ubicada cerca de Costa Rica, un empleado de InGen (Tecnologías Genéticas Internacionales, Inc., por sus siglas en inglés) es atacado por un misterioso y letal animal, al cual pretendía liberar de su caja en su futuro hábitat en el reciente parque zoológico en construcción. Su muerte origina un conflicto entre la familia del empleado asesinado e InGen. Como consecuencia, su gerente John Hammond es presionado por los inversores del proyecto, para permitir una investigación segura por expertos en la isla, antes de la apertura del parque. Hammond visita al paleontólogo Alan Grant y a la paleobotánica Ellie Sattler, en su campamento arqueológico y les propone financiar sus estudios algunos años a cambio que sean parte del comité de evaluación, es así como acaban viajando a la isla en compañía del matemático Ian Malcolm y el representante de los inversores Donald Gennaro. </b><br />
+           <b style={{ justifyContent: 'center', fontSize: '15px', fontFamily: 'Georgia, serif' }}>Al entrar en el parque, los investigadores conocen a un ejemplar de un Brachiosaurus, revelándose solo en este momento a ellos la verdadera naturaleza del parque; mientras que en el interior de los laboratorios se les informa sobre la técnica empleada por los científicos para recrear a las diferentes especies de dinosaurios. InGen creó a los dinosaurios a partir de la clonación genética de materia fósil hallada en mosquitos prehistóricos que se alimentaron de la sangre de dinosaurios durante la época jurásica, preservados en ámbar a través del transcurso del tiempo. El ADN de esos especímenes fue terminado con la adición de segmentos genéticos provenientes de ranas, con tal de reponer las secciones originales dañadas por el paso del tiempo. Durante el proceso, fueron creados sólo dinosaurios hembra estériles para prevenir su procreación y una posible e incontrolada sobrepoblación de dinosaurios en la isla, también se les diseñó con una falla endocrina que los hace incapaces de generar lisina, por lo que si no la consumen en sus alimentos en horas caerán en coma y morirán, de esta forma se ha pensado posible evitar una fuga exitosa. </b><br />
+          </p>
+        </div>
+      </div>
+      <hr></hr>
+
+      <div>
+        <div style={{ border: '2px solid black', borderRadius: '10px', padding: '10px', backgroundColor: '#f2f2f2' }}>
+          <h1 style={{ justifyContent: 'center',  fontSize: '30px', textAlign: 'center', fontFamily: 'Times New Roman, Times, serif' }}> Dinosaurios en pantalla</h1>
+          <p>
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif' }}>*Brachiosaurus.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif' }}>*Dilophosaurus.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif' }}>*Gallimimus.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif' }}>*Parasaurolophus.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>*Triceratops.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>*Tyrannosaurus Rex.</b><br />
+            <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>*Velociraptor.</b><br />
+          </p>
+        </div>
+      </div>
+      <hr></hr>
+  
+      <div style={{ border: '2px solid black', borderRadius: '10px', padding: '20px', backgroundColor: '#f2f2f2'}}>
+      <h1 style={{ justifyContent: 'center',  fontSize: '30px', textAlign: 'center', fontFamily: 'Times New Roman, Times, serif' }}> Reparto</h1>
+        <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>Sam Neill como Alan Grant.</b><br />    
+        <img src="https://cdn.discordapp.com/attachments/801085103274328095/1231746211917332560/latest.png?ex=6638140d&is=66259f0d&hm=0395bc5ea38fb1280dc1b56deeda52b64dd13868a1c0652553e52c064671ff2e&" style={{ height: '150px' }} alt="Zzz" />
+        <b></b><br />
+
+        <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>Laura Dern como Ellie Sattler.</b><br /> 
+        <img src="https://cdn.discordapp.com/attachments/801085103274328095/1231746080673234944/latest.png?ex=663813ee&is=66259eee&hm=9aaa386fbf4d28ec6e111cb2d53d134ff9e121a2a17c3231bc3871f5d6b0872a&" style={{ height: '150px' }} alt="Zzz" />
+        <b></b><br />
+
+        <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>Jeff Goldblum como Ian Malcolm.</b><br />
+        <img src="https://cdn.discordapp.com/attachments/801085103274328095/1231746141641511024/latest.png?ex=663813fc&is=66259efc&hm=73d645c001a434275b742b058036a7ff52e5c21b1ed85eaa9f2710818d8d5827&" style={{ height: '150px' }} alt="Zzz" />
+        <b></b><br />
+
+        <b style={{ fontSize: '15px', fontFamily: 'Georgia, serif'}}>Richard Attenborough como John Hammond.</b><br />
+        <img src="https://cdn.discordapp.com/attachments/801085103274328095/1231746151829471312/latest.png?ex=663813ff&is=66259eff&hm=f8c3c268ef5f744b80f35b4ca715a47c89d505fa58c70e3a9dee543e57b0d375&" style={{ height: '150px' }} alt="Zzz" />
+        <b></b><br />
+
+      </div>
+
+    </div>
+      </h1>
+    </div>
+  );
+}
+
+export default Intraclase;
